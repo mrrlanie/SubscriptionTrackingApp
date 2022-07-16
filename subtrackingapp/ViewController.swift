43725dc1a278320ac857d59a789eed5ff6e48eea
Darya Pakhomova <mrrlanie@gmail.com>
2022-07-16 04:47:49 -0700

    Initial Commit

---
+++ subtrackingapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  subtrackingapp
+//
+//  Created by Администратор on 16.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
